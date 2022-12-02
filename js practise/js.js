6400dcc667c79cb0966fa589a5c4b64d1005a933
kd <ms0658877@gmail.com>
2022-12-02 16:40:24 +0800

    0

--- js practise/js.js
+++ js practise/js.js
@@ -1,2 +1,16 @@
+let searchBtn=document.querySelector('.searchBtn');
+let cloaseBtn=document.querySelector('.closeBtn');
+let searchBox=document.querySelector('.searchBox');
 
-alert("你好");
+searchBtn.onclick = function(){
+    searchBox.classList.add('active');
+    cloaseBtn.classList.add('active');
+    searchBtn.classList.add('active');
+}
+
+
+cloaseBtn.onclick = function(){
+    searchBox.classList.remove('active');
+    cloaseBtn.classList.remove('active');
+    searchBtn.classList.remove('active');
+}
